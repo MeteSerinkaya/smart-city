8864725e2b469146bd11cf704157377aec3c60ad
MeteSerinkaya <meteserinkaya@gmail.com>
2025-08-06 13:43:58 +0300

    Fix AppConstants.baseUrl compilation error

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -1,6 +1,8 @@
 class AppConstants {
   static const LANG_ASSET_PATH = "asset/lang";
   static const IPAD_NAME = "IPAD";
-
   static const COMPANY_NAME = "METE";
+  
+  // API Base URL
+  static const String baseUrl = "https://localhost:7276/api/";
 }
